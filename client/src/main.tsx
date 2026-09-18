76f7412a0e522d13e6806a5eddf73917855db740
binarygeek119 <binarygeek119@gmail.com>
2026-09-18 12:55:12 -0500

    Use Red Hat Display for the whole YAQ UI.
    Self-host the typeface and drop Google Fonts / Anybody / IBM Plex so headings, body, and form controls share YARG's display font on the LAN.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/red-hat-display/400.css";
+import "@fontsource/red-hat-display/500.css";
+import "@fontsource/red-hat-display/600.css";
+import "@fontsource/red-hat-display/700.css";
+import "@fontsource/red-hat-display/800.css";
+import "@fontsource/red-hat-display/900.css";
 import App from "./App.tsx";
 import "./index.css";
 
